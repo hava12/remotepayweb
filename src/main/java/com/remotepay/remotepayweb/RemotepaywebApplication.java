8b04a367da844f714149ea3ca5f41d7218409ebc
dongyeop <hava12@naver.com>
2021-12-16 22:26:32 +0900

    Initial commit

---
+++ src/main/java/com/remotepay/remotepayweb/RemotepaywebApplication.java
@@ -0,0 +1,13 @@
+package com.remotepay.remotepayweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemotepaywebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemotepaywebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/remotepay/remotepayweb/RemotepaywebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remotepay.remotepayweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemotepaywebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
